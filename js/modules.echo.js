4f56addf1b5fa9ad83776ceb85a3125ff317600e
Jinhyeok Lee <zenyr@zenyr.com>
2013-08-01 16:56:31 +0900

    fixed Echo anomaly

--- js/modules.echo.js
+++ js/modules.echo.js
@@ -1,6 +1,7 @@
 AppModules.Echo = function (self) {
   var list = [];
   return function(msg){
+    list.reverse();
     list.push((new Date()).toLocaleTimeString()+':'+msg);
     if(list.length>3) list=list.slice(-3);
     $('#echo').html('<p>'+list.reverse().join('<p>'));
